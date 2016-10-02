8368004c78c1555f75c2f0a539f8be3d86666230
ckhan89 <caokyhan@gmail.com>
2016-10-03 01:36:17 +0700

    Update challenge3.sql

--- challenge3.sql
+++ challenge3.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT created_at as month,(SELECT (
 	(SELECT COUNT(DISTINCT user_id) FROM PURCHASES where MONTH(created_at) =  MONTH(month))
 	- 
-	(SELECT COUNT(DISTINCT user_id) FROM PURCHASES where created_at < month and user_id in (SELECT user_id FROM PURCHASES where MONTH(created_at) =  MONTH(month)))
+	(SELECT COUNT(DISTINCT user_id) FROM PURCHASES where MONTH(created_at) < MONTH(month) and user_id in (SELECT user_id FROM PURCHASES where MONTH(created_at) =  MONTH(month)))
 	)
 ) as first_time_buyers,
-	(SELECT COUNT(DISTINCT user_id) FROM PURCHASES where created_at < month and user_id in (SELECT user_id FROM PURCHASES where MONTH(created_at) =  MONTH(month))) as returned_buyers
+	(SELECT COUNT(DISTINCT user_id) FROM PURCHASES where MONTH(created_at) < MONTH(month) and user_id in (SELECT user_id FROM PURCHASES where MONTH(created_at) =  MONTH(month))) as returned_buyers
 from purchases;
